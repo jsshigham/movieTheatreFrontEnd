70ff8d17d5a1fe75dffe6cab3baccb3a10675406
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2023-09-21 08:56:55 -0500

    cols showtime

--- src/components/Showtimes.jsx
+++ src/components/Showtimes.jsx
@@ -1,35 +1,33 @@
 import React from "react";
 
 const Showtimes = ({ movie }) => {
-  
-
   return (
-    <div className="flex  rounded p-2 w-full items-center justify-center gap-2">
-      <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+    <div className="grid grid-cols-3 rounded p-2 w-full items-center justify-center gap-2 ">
+      <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
         {movie.showTime1}
       </div>
-      {!movie.showTime2 ? null : (
-        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+      {movie.showTime2 && (
+        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
           {movie.showTime2}
         </div>
       )}
-      {!movie.showTime3 ? null : (
-        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+      {movie.showTime3 && (
+        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
           {movie.showTime3}
         </div>
       )}
-      {!movie.showTime4 ? null : (
-        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+      {movie.showTime4 && (
+        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
           {movie.showTime4}
         </div>
       )}
-      {!movie.showTime5 ? null : (
-        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+      {movie.showTime5 && (
+        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
           {movie.showTime5}
         </div>
       )}
-      {!movie.showTime6 ? null : (
-        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white">
+      {movie.showTime6 && (
+        <div className="flex items-center justify-center bg-sky-950 p-1 rounded text-white h-full">
           {movie.showTime6}
         </div>
       )}
